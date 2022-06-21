b0cda2ccf5b2ad66998286d027b263234e7e41a2
qaximalee <qasim@vinncorp.com>
2022-06-21 15:11:22 +0500

    added new files

---
+++ src/main/java/com/clickpay/ClickPayApplication.java
@@ -0,0 +1,13 @@
+package com.clickpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clickpay/ClickPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clickpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
